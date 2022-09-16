37cb54c0442c1899dc037b94b0e2ba64eea15219
Ilya Apaniuk <i.apaniuk@reply.com>
2022-09-16 14:20:24 +0200

    new model

---
+++ SharePointPermissionsResolver/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SharePointPermissionsResolver.Models
+{
+    public class ApiResponse
+    {
+        public string? Content { get; set; }
+
+        public bool IsSuccessed { get; set; }
+    }
+}
+
